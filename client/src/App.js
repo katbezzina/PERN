767313fab3e5b975297c4a0fd70556293fcc3e1c
katia_bezzina <katia.bezzina@gmail.com>
2022-11-07 17:19:04 +0100

    Landing Page test

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import LandingPage from "./Views/LandingPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
 }
 
 export default App;
