e079df31e08c6e88fde702e9ea2678d40f6fc745
katia_bezzina <katia.bezzina@gmail.com>
2022-11-08 17:25:01 +0100

    Landing Page, navigation bar not in place

--- client/src/Views/LandingPage.jsx
+++ client/src/Views/LandingPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import NavigationMenu from '../Components/NavigationMenu'
+import img from "../salad.png"
+import "../Style/LandingPage.css"
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className="landingpageBackground">
+      <img src={img} alt="Logo" className="landingpageLogo" />
+      <NavigationMenu/>
+    </div>
   )
 }
 
