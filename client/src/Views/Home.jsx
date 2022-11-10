3443c9a19e031340a8ad803df66579ab5e488f81
katia_bezzina <katia.bezzina@gmail.com>
2022-11-10 16:33:40 +0100

    updating url in fetch

--- client/src/Views/Home.jsx
+++ client/src/Views/Home.jsx
@@ -3,7 +3,7 @@ import React, {useEffect} from 'react'
 const Home = () => {
 const getUsers = async () => {
     try {
-      const response = await fetch("http://localhost:5000/rawquery");
+      const response = await fetch("http://localhost:5000/users/all");
       const jsonDATA = await response.json();
       console.log(jsonDATA);
     } catch (error) {
