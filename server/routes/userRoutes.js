91c138240d837c98376258788def02650826030a
katia_bezzina <katia.bezzina@gmail.com>
2022-11-21 13:40:52 +0100

    validating info for userRoutes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,6 +5,7 @@ import {
   register,
   login,
 } from "../controllers/usersController.js";
+import validInfo from "../middleware/validInfo.js";
 // import pool from "../dbConfig.js";
 // import sequelize from "../dbConfig.js";
 
@@ -12,8 +13,10 @@ const router = express.Router();
 
 router.get("/all", getAllUsers);
 router.get("/:id", getUserById);
-router.post("/register", register);
-router.post("/login", login);
+
+//adding a validation middleware
+router.post("/register", validInfo, register);
+router.post("/login", validInfo, login);
 
 // //GET route to query users table with Sequelize
 // router.get("/all", async (req, res) => {
