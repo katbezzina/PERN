6e4274331d905cb3b40cdcbe6764bdd60819c0a7
katia_bezzina <katia.bezzina@gmail.com>
2022-11-23 17:08:55 +0100

    Logour modal using material ui

--- client/src/Components/Logout.js
+++ client/src/Components/Logout.js
@@ -1,40 +1,58 @@
 import React, { useState, useContext } from "react";
 import ListItemIcon from "@mui/material/ListItemIcon";
-import Modal from "react-bootstrap/Modal";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../Context/AuthContext";
 import "../Style/NavigationMenu.css";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 300,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
+
 function Logout(props) {
   const { logout } = useContext(AuthContext);
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <>
-      <ListItemIcon onClick={handleShow}>Logout</ListItemIcon>
-      <Modal show={show} onHide={handleClose} animation={false}>
-        {/* <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        show={show}
-        onHide={handleClose}
-      > */}
-        <Modal.Header closeButton></Modal.Header>
-        <Modal.Body>Are you sure you would like to log out?</Modal.Body>
-        <Modal.Footer>
-          <button onClick={handleClose} className="noUnderline registerButton">
-            No
-          </button>
-          <Link to="/Login">
-            <button onClick={logout} className="noUnderline loginButton">
-              Logout anyway!
+      <ListItemIcon onClick={handleOpen}>Logout</ListItemIcon>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Are you sure you would like to log out?
+          </Typography>
+          <br />
+          <br />
+          <div className="buttons">
+            <button
+              onClick={handleClose}
+              className="noUnderline registerButton"
+            >
+              No
             </button>
-          </Link>
-        </Modal.Footer>
+            <Link to="/Login">
+              <button onClick={logout} className="noUnderline loginButton">
+                Logout anyway!
+              </button>
+            </Link>
+          </div>
+        </Box>
       </Modal>
     </>
   );
